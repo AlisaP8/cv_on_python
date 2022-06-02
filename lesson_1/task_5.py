98ec70fae50e5b4835d6b927dfea234f7660a9ae
AlisaP8 <78349377+AlisaP8@users.noreply.github.com>
2022-06-03 00:25:59 +0400

    lesson_1

--- lesson_1/task_5.py
+++ lesson_1/task_5.py
@@ -1,13 +1,13 @@
 from lesson_1.task_4 import total_sum, start_sum, perc, func, summ, month
 
 quantity = int(input('Введите сумму пополнения \n'))
+print(f'Сумма пополнения: {quantity}р. ежемесячно')
 
 
 def get_summ(start_sum, month, quan):
     i = 0
     if quan == 0:
-        res = perc(func)
-        return res
+        return
 
     while True:
         for i in range(month - 1):
@@ -25,9 +25,13 @@ def quantity(val, percent):
         res = start_sum / 100.0 * percent / 12
         item = val / (month - 2) / 100.0 * res / 12
         result = res * month + item + start_sum + val
-        print(f'Итоговая сумма через {month}', 'месяцев составит {:.2f} рублей'.format(result))
+        print(f'Итоговая сумма, с учетом пополнения, через {month}', 'месяцев составит {:.2f} рублей'.format(result))
     except TypeError:
         print('Некорректные данные')
+    # return
 
 
-quantity(summ_quan, res)
+if summ_quan != None:
+    quantity(summ_quan, res)
+else:
+    perc(func)
